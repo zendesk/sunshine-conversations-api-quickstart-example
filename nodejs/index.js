7a85cfbda6b936b0ffcd5a02c0e835e878b3de8b
spasiu <stasi@smooch.io>
2016-12-22 16:15:54 -0500

    Take out superagent

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,21 +3,17 @@
 // Imports
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const superagent = require('superagent');
+const Smooch = require('smooch-core');
 
 // Config
 const PORT = 8000;
 const KEY_ID = 'your_key_id';
 const SECRET = 'your_secret_key';
 
-// JWT https://docs.smooch.io/rest/#jwt
-const authToken = jwt.sign({ scope: 'app' }, SECRET, {
-    header: {
-        typ: 'JWT',
-        kid: KEY_ID,
-        alg: 'HS256'
-    }
+const smooch = new Smooch({
+    keyId: KEY_ID,
+    secret: SECRET,
+    scope: 'app'
 });
 
 // Server https://expressjs.com/en/guide/routing.html
@@ -32,18 +28,19 @@ app.post('/message', function(req, res) {
   const appUserId = req.body.appUser._id;
   // Call REST API to send message https://docs.smooch.io/rest/#post-message
   if (req.body.trigger === 'message:appUser') {
-    superagent
-    .post(`https://app.smooch.io/v1/appusers/${appUserId}/conversation/messages`)
-    .send({
-        text: 'Live long and prosper',
-        role: 'appMaker'
-    })
-    .set('authorization', `Bearer ${authToken}`)
-    .set('Accept', 'application/json')
-    .end(function(err, response) {
-        console.log('API RESPONSE:\n', err, response.body, response.statusCode);
-        res.end();
-    });
+      smooch.appUsers.sendMessage(appUserId, {
+          type: 'text',
+          text: 'Live long and prosper',
+          role: 'appMaker'
+      })
+          .then((response) => {
+              console.log('API RESPONSE:\n', response);
+              res.end();
+          })
+          .catch((err) => {
+              console.log('API ERROR:\n', err);
+              res.end();
+          });
   }
 });
 
