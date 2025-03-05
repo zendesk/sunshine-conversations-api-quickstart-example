09f4f1902baf278a953d40930200218e30a139b2
Mike Spensieri <mikespensieri@smooch.io>
2025-03-05 11:53:25 -0500

    Update for zendesk quickstart

--- README.md
+++ README.md
@@ -2,16 +2,15 @@
 
 ## Get started
 
-For a detailed guide, see the [Sunshine Conversations API Quickstart](https://docs.smooch.io/guide/api-quickstart):
+For a detailed guide, see the [Sunshine Conversations API Quickstart](https://developer.zendesk.com/documentation/conversations/getting-started/api-quickstart/):
 
 ### Node.js
 
 1. Clone the repository
 2. Go to the _nodejs_ subdirectory
-3. Update index.js to include your secret key and key ID from your [Sunshine Conversations](https://app.smooch.io) settings
-4. Install dependencies (`npm install`)
-5. Run the server (`node index`)
-6. Use [ngrok](https://ngrok.com/) to create a secure tunnel to port 8000(`ngrok http 8000` after ngrok is installed on your PC)
-7. Create a Facebook page and [connect it to Sunshine Conversations](https://app.smooch.io/integrations/messenger)
-8. Create a Webhook from your [dashboard](https://app.smooch.io/integrations/webhook) and point it at the full url for the /messages endpoint (e.g. https://MY-NGROK-DOMAIN.ngrok.io/messages )
-9. Send messages to your Facebook page and watch the auto-replies roll in
+3. Install dependencies (`npm install`)
+4. Use [ngrok](https://ngrok.com/) to create a secure tunnel to port 8000 (`ngrok http 8000` after ngrok is installed on your PC)
+5. Create a webhook and API key in Admin Center and point it at the full url for the `/messages` endpoint (e.g. `https://MY-NGROK-DOMAIN.ngrok.io/messages`)
+6. Update `index.js` to set proper values for `ZENDESK_SUBDOMAIN`, `KEY_ID` and `KEY_SECRET`
+7. Run the server (`node index`)
+8. Send messages to your Web Widget and watch the auto-replies roll in
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,19 +1,24 @@
-'use strict';
+"use strict";
 
 // Imports
-const express = require('express');
-const bodyParser = require('body-parser');
-const SunshineConversationsApi = require('sunshine-conversations-client');
+const express = require("express");
+const bodyParser = require("body-parser");
+const SunshineConversationsApi = require("sunshine-conversations-client");
 
+const ZENDESK_SUBDOMAIN = "{subdomain}.zendesk.com";
+const KEY_ID = "{key-id}";
+const KEY_SECRET = "{key-secret}";
 
 // Config
 let defaultClient = SunshineConversationsApi.ApiClient.instance;
-let basicAuth = defaultClient.authentications['basicAuth'];
-basicAuth.username = 'your_key_id';
-basicAuth.password = 'your_secret_key';
+defaultClient.basePath = `https://${ZENDESK_SUBDOMAIN}/sc`;
+
+let basicAuth = defaultClient.authentications["basicAuth"];
+basicAuth.username = KEY_ID;
+basicAuth.password = KEY_SECRET;
 const PORT = 8000;
 
-const apiInstance = new SunshineConversationsApi.MessagesApi()
+const apiInstance = new SunshineConversationsApi.MessagesApi();
 
 // Server https://expressjs.com/en/guide/routing.html
 const app = express();
@@ -21,21 +26,34 @@ const app = express();
 app.use(bodyParser.json());
 
 // Expose /messages endpoint to capture webhooks https://docs.smooch.io/rest/#operation/eventWebhooks
-app.post('/messages', function(req, res) {
-  console.log('webhook PAYLOAD:\n', JSON.stringify(req.body, null, 4));
-
+app.post("/messages", async function (req, res) {
   const appId = req.body.app.id;
-  const trigger = req.body.events[0].type;
+  const [event] = req.body.events;
 
   // Call REST API to send message https://docs.smooch.io/rest/#operation/postMessage
-  if (trigger === 'conversation:message') {
-    const authorType = req.body.events[0].payload.message.author.type;
-    if(authorType === 'user'){
-        const conversationId = req.body.events[0].payload.conversation.id;
-        sendMessage(appId, conversationId);
-        res.end();
+  if (event.type === "conversation:message") {
+    const { conversation, message } = event.payload;
+
+    if (message.author.type === "user") {
+      console.log(
+        "User message received:\n",
+        JSON.stringify(req.body, null, 4)
+      );
+
+      try {
+        await sendMessage(
+          appId,
+          conversation.id,
+          `You said: ${message.content.text}`
+        );
+        console.log("Message sent successfully");
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
     }
   }
+
+  res.end();
 });
 
 // Listen on port
@@ -43,10 +61,14 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
-async function sendMessage(appId, conversationId){
-    let messagePost = new SunshineConversationsApi.MessagePost();  
-    messagePost.setAuthor({type: 'business'});
-    messagePost.setContent({type: 'text', text: 'Live long and prosper'});
-    let response = await apiInstance.postMessage(appId, conversationId, messagePost);
-    console.log('API RESPONSE:\n', response);
+async function sendMessage(appId, conversationId, text) {
+  const messagePost = new SunshineConversationsApi.MessagePost();
+  messagePost.setAuthor({ type: "business" });
+  messagePost.setContent({ type: "text", text });
+  const response = await apiInstance.postMessage(
+    appId,
+    conversationId,
+    messagePost
+  );
+  console.log("API RESPONSE:\n", JSON.stringify(response, null, 4));
 }
